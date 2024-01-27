0c3300fab4dc5d9191118f9fe48962ca9463e24d
VaibhavRtd7 <vaibhavrathod1902@gmail.com>
2024-01-28 02:40:05 +0530

    Add industry STD. api

--- 2-backend/index.js
+++ 2-backend/index.js
@@ -3,11 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/jokes', (req, res) => { 
+app.get('/api/jokes', (req, res) => { 
 
     const jokes = [
         {
